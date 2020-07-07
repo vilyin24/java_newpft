63af6f340acaa6c5804ebfce02063f90bca92bba
vilyin24 <valerilin12@gmail.com>
2020-07-08 01:01:40 +0300

    6.2 точно предедущий 6.1

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGeneration.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGeneration.java
@@ -3,14 +3,37 @@ package ru.stqa.pft.addressbook.generators;
 import ru.stqa.pft.addressbook.model.GroupDate;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGeneration {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int count = Integer.parseInt(args[0]);
         File file = new File(args[1]);
 
-        List<GroupDate> group = generateGroups(count);
+        List<GroupDate> groups = generateGroups(count);
+        save(groups, file);
+    }
+
+    private static void save(List<GroupDate> groups, File file) throws IOException {
+        System.out.println(new File(".").getAbsolutePath());
+        Writer writer = new FileWriter(file);
+        for (GroupDate group: groups) {
+        writer.write(String.format("%s;%s;%s\n",group.getName(),group.getHeader(),group.getFooter()));
+        }
+        writer.close();
+    }
+
+    private static List<GroupDate> generateGroups(int count) {
+        List<GroupDate> groups = new ArrayList<GroupDate>();
+        for (int i = 0 ; i <count;i++){
+            groups.add(new GroupDate().withName(String.format("test %s", i))
+                    .withHeader(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
+        }
+            return groups;
     }
 }
