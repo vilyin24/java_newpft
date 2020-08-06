5401d2d5a9f82dd00b30fe126dc56fab4a55e247
vilyin24 <valerilin12@gmail.com>
2020-08-06 19:36:41 +0300

    8.2

---
+++ mantis-tests/src/main/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+public class TestBase {
+
+    protected static  final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws Exception {
+        app.stop();
+    }
+}
