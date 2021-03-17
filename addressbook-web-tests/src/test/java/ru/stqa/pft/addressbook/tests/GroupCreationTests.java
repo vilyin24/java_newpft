aa87a7ae52ad2821772eaeb26a090c9fcf8a8246
Valery <valerilin12@gmail.com>
2021-03-18 00:10:14 +0300

    6.5 вытаскивам из файла тестовые данные

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -7,9 +7,9 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupDate;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -18,17 +18,28 @@ import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTests extends TestBase{
 
+@DataProvider
+public Iterator<Object[]> validGroups() throws IOException {
+  List<Object[]> list = new ArrayList<Object[]>();
+  BufferedReader reader = new BufferedReader(new FileReader(new File("src/resources/groups.csv")));
+  String line = reader.readLine();
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  while (line != null){
+    String[] split = line.split(";");
+    list.add(new Object[] {new GroupDate().withName(split [0]).withHeader(split [1]).withFooter(split [2])});
+    line = reader.readLine();
+  }
+  return list.iterator();
+}
+
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupDate group)  {
     app.goTo().GroupPage();
     Groups before = app.group().all();
-    GroupDate group = new GroupDate().withName("test2");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
-    assertThat(after.size(), equalTo(before.size() + 1));
-
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) ->g.getId()).max().getAsInt()))));
   }
   @Test
