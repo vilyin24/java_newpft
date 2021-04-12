65a3f5b346a1d636ed41c9465179d695e4af4d6b
Valery <valerilin12@gmail.com>
2021-04-12 22:45:57 +0300

    7.3 без домашки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactDate.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactDate.java
@@ -2,35 +2,76 @@ package ru.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name ="addressbook")
 public class  ContactDate {
-    private  int id;
+    @Id
+    @Column(name ="id")
+    private int id = Integer.MAX_VALUE;
+
     @Expose
-    private  String fistname;
+    @Column(name ="firstname")
+    private  String firstname;
+
+    @Expose
+    @Column(name ="middlename")
     private  String middlename;
+
     @Expose
+    @Column(name ="lastname")
     private  String lastname;
-    @Expose
+    //@Expose
+
+    @Transient
     private String group;
+
+    @Column(name ="home")
+    @Type(type = "text")
     private String homePhone;
+
+    @Column(name ="mobile")
+    @Type(type = "text")
     private String mobilePhone;
+
+    @Type(type = "text")
+    @Column(name ="work")
     private String workPhone;
+
+    @Transient
     private String allPhones;
+
+    @Column(name = "email")
+    @Type(type = "text")
     private String email;
+
+    @Column(name = "email2")
+    @Type(type = "text")
     private String email2;
+
+    @Column(name = "email3")
+    @Type(type = "text")
     private String email3;
-    private File photo;
+
+    @Column(name ="photo")
+    @Type(type = "text")
+    private String photo;
+
+    @Transient
+    private String allEmails;
 
     public File getPhoto() {
-        return photo;
+        return new File (photo);
     }
 
     public ContactDate withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
@@ -43,7 +84,15 @@ public class  ContactDate {
         return this;
     }
 
-    private String allEmails;
+    @Override
+    public String toString() {
+        return "ContactDate{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", middlename='" + middlename + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
 
     public String getEmail() {
         return email;
@@ -100,14 +149,6 @@ public class  ContactDate {
 
     }
 
-    @Override
-    public String toString() {
-        return "ContactDate{" +
-                "id=" + id +
-                ", lastname='" + lastname + '\'' +
-                '}';
-    }
-
 
     @Override
     public boolean equals(Object o) {
@@ -129,7 +170,7 @@ public class  ContactDate {
     }
 
     public ContactDate withFistname(String fistname) {
-        this.fistname = fistname;
+        this.firstname = fistname;
         return this;
     }
 
@@ -149,7 +190,7 @@ public class  ContactDate {
     }
 
     public String getFistname() {
-        return fistname;
+        return firstname;
     }
 
     public String getMiddlename() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,47 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactDate;
+import ru.stqa.pft.addressbook.model.GroupDate;
+
+import java.util.List;
+
+public class HbConnectionTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+           e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+
+    @Test
+    public void testHbConnection (){
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactDate> result = session.createQuery( "from ContactDate where deprecated ='0000-00-00'" ).list();
+        for ( ContactDate contact: result ) {
+            System.out.println(contact);
+        }
+        session.getTransaction().commit();
+        session.close();
+
+    }
+}
